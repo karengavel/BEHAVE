2da6e9937bfc79595fefbf87df8c3784d0ae4a46
Karen Gama <kariigavel@gmail.com>
2023-02-28 18:00:24 -0600

    se agregan cambios de login

--- Behave/steps/side_menu/login_in_steps.py
+++ Behave/steps/side_menu/login_in_steps.py
@@ -2,19 +2,19 @@ from behave import *
 from screens.side_menu.log_in_screen import LogInScreen
 
 
-@Then('We enter the correct user')
+@When('We enter the correct user')
 def step_impl(context):
     log_in = LogInScreen(context)
-    log_in.fill_text(*log_in.txt_username, text=context.STANDARD_USER)
+    log_in.fill_text(*log_in.txt_username, text='bob@example.com')
 
 
-@Then('We enter the correct password')
+@When('We enter the correct password')
 def step_impl(context):
     log_in = LogInScreen(context)
-    log_in.fill_text(*log_in.txt_password, text=context.PASSWORD)
+    log_in.fill_text(*log_in.txt_password, text="10203040")
 
 
-@Then('Tap on the Log In button')
+@When('Tap on the Log In button')
 def step_impl(context):
     log_in = LogInScreen(context)
     log_in.tap_element(*log_in.btn_login)
